8bec1c8e717eb8edc29ddc01f89e52c7743941fb
zxycode007 <373982141@qq.com>
2017-11-14 10:11:10 +0800

    增加GamePlayer

---
+++ GamePlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class GamePlayer
+    {
+        int m_playerID;
+        string m_playerName;
+        string m_playerIcon;
+
+        public int PlayerID
+        {
+            get
+            {
+                return m_playerID;
+            }
+            set
+            {
+                m_playerID = value;
+            }
+        }
+
+        public string PlayerName
+        {
+            get
+            {
+                return m_playerName;
+            }
+            set
+            {
+                m_playerName = value;
+            }
+        }
+
+        public string PlayerIcon
+        {
+            get
+            {
+                return m_playerIcon;
+            }
+            set
+            {
+                m_playerIcon = value;
+            }
+        }
+    }
+}
